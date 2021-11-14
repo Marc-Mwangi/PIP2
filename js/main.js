659557f051c993efb3df3dc52402ba0ca56bd5d0
MarcMwangi <marc.mwangi@student.moringaschool.com>
2021-11-14 12:31:44 +0300

    Creating arrays of names and dates in javascript

---
+++ js/main.js
@@ -0,0 +1,4 @@
+akanNamesboy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yao", "Kofi", "Kwame"];
+akanNamesgirl = ["Akosua", "Adwoa", "Abena","Akua","Yaa", "Afua", "Amma"];
+days = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[0]);
