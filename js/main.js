bc87754d1f26f5e98cf285e618b27fa92e34b3b1
MarcMwangi <marc.mwangi@student.moringaschool.com>
2021-11-14 14:48:23 +0300

    Fixing gender form layout and saving its output

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 akanNamesboy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yao", "Kofi", "Kwame"];
 akanNamesgirl = ["Akosua", "Adwoa", "Abena","Akua","Yaa", "Afua", "Amma"];
 days = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var gender = document.getElementsByName("sex");
-var date = document.getElementById("#birthday");
+var gen ;
+const form = document.getElementById("data");
+ form.addEventListener("submit", (e) => {
+     e.preventDefault();
+     var boy = document.getElementById("Male").value ;
+     var girl = document.getElementById("Female").value;
+     var gender =form[0].value;
+     for(var i = 0; i < form.length; i++){
+         if(form[i].type ==="radio" && form[i].checked)
+         gen = form[i].value;
+         console.log(gen)
+     }
+    
+ })
 
-console.log(gender);
