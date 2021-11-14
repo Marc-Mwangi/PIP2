acddac3bc49a46cdd018e4cdf0548a055492865f
MarcMwangi <marc.mwangi@student.moringaschool.com>
2021-11-14 13:27:48 +0300

    Inserting gender forms

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 akanNamesboy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yao", "Kofi", "Kwame"];
 akanNamesgirl = ["Akosua", "Adwoa", "Abena","Akua","Yaa", "Afua", "Amma"];
 days = ["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"];
-console.log(days[0]);
+var gender = document.getElementsByName("sex");
+var date = document.getElementById("#birthday");
+
+console.log(gender);
