b9963b3049936ea41a3022950ab0de113748ca70
MarcMwangi <marc.mwangi@student.moringaschool.com>
2021-11-14 14:55:25 +0300

    saving and assigning outputs of forms in js

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ var gen ;
 const form = document.getElementById("data");
  form.addEventListener("submit", (e) => {
      e.preventDefault();
+     var date = document.getElementById("birthday").value ;
      var boy = document.getElementById("Male").value ;
      var girl = document.getElementById("Female").value;
      var gender =form[0].value;
      for(var i = 0; i < form.length; i++){
          if(form[i].type ==="radio" && form[i].checked)
          gen = form[i].value;
-         console.log(gen)
+         console.log(gen);
+         console.log(date);
      }
     
  })
